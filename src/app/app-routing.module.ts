c691fd70686bfc1111a99f2e7e3ec7845f06d7b2
Robert Buj <robert.buj@gmail.com>
2025-01-17 19:13:33 +0100

    create the modules home, list and statistics
    ng g m home --route home --module app.module --routing
    ng g m list --route list --module app.module --routing
    ng g m statistics --route statistics --module app.module --routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then((m) => m.ListModule)
+  },
+  {
+    path: 'statistics',
+    loadChildren: () =>
+      import('./statistics/statistics.module').then((m) => m.StatisticsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
