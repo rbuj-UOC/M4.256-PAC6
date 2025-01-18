a45faf034579509fe2a9872adb62898525ee75a8
Robert Buj <robert.buj@gmail.com>
2025-01-18 14:21:43 +0100

    statistics module
    ng g c statistics/statistics-page -m statistics
    ng g c statistics/resume-data -m statistics
    ng g c statistics/gender -m statistics
    ng g c statistics/general-results -m statistics

---
+++ src/app/statistics/general-results/general-results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-results',
+  templateUrl: './general-results.component.html',
+  styleUrl: './general-results.component.scss'
+})
+export class GeneralResultsComponent {
+
+}
--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { GenderComponent } from './gender/gender.component';
+import { GeneralResultsComponent } from './general-results/general-results.component';
+import { ResumeDataComponent } from './resume-data/resume-data.component';
+import { StatisticsPageComponent } from './statistics-page/statistics-page.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
-import { StatisticsComponent } from './statistics.component';
-
 
 @NgModule({
   declarations: [
-    StatisticsComponent
+    StatisticsPageComponent,
+    ResumeDataComponent,
+    GenderComponent,
+    GeneralResultsComponent
   ],
-  imports: [
-    CommonModule,
-    StatisticsRoutingModule
-  ]
+  imports: [CommonModule, StatisticsRoutingModule]
 })
-export class StatisticsModule { }
+export class StatisticsModule {}
